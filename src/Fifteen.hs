1599f9416b101baf192b79d7260efaa1f8f25021
Shaun Lee <shaun.lee@gmail.com>
2019-01-06 14:46:15 -0800

    Switch to Map representation of creatures. Faster, but still slow.

--- src/Fifteen.hs
+++ src/Fifteen.hs
@@ -59,7 +59,7 @@ instance Show Creature where
   show (Creature Goblin _ hp _) = "G(" ++ (show hp) ++ ")"
   show (Creature Elf _ hp _)    = "E(" ++ (show hp) ++ ")"
 
-type Creatures = Set Creature
+type Creatures = Map Pos Creature
 
 type Board = Map Pos Square
 
@@ -88,15 +88,10 @@ instance Show GameState where
     in concatMap (\yrow -> showRow yrow) [ymin .. ymax]
 
 findCreature :: Creatures -> Pos -> Maybe Creature
-findCreature cs pos =
-  let found = S.filter (\c -> cpos c == pos) cs
-  in if S.null found
-       then Nothing
-       else Just $ S.elemAt 0 found
+findCreature cs pos = cs M.!? pos
 
 creaturesInRow :: Creatures -> Int -> [Creature]
-creaturesInRow cs y =
-  S.toList $ S.filter (\c@(Creature _ (Pos (_, yc)) _ _) -> yc == y) cs
+creaturesInRow cs y = M.elems $ M.filterWithKey (\(Pos (_, yy)) _ -> y == yy) cs
 
 parseSquare :: Pos -> Char -> ((Pos, Square), Maybe Creature)
 parseSquare p '#' = ((p, Wall), Nothing)
@@ -104,11 +99,11 @@ parseSquare p '.' = ((p, Open), Nothing)
 parseSquare p 'G' = ((p, Open), Just $ Creature Goblin p 200 p)
 parseSquare p 'E' = ((p, Open), Just $ Creature Elf p 200 p)
 
-parseRow :: Int -> String -> ([(Pos, Square)], [Creature])
+parseRow :: Int -> String -> ([(Pos, Square)], [(Pos, Creature)])
 parseRow y rowS =
   let parsedrow = map (\(x, c) -> parseSquare (Pos (x, y)) c) (zip [0 ..] rowS)
       squares = map fst parsedrow
-      creatures = catMaybes $ map snd parsedrow
+      creatures = map (\c -> (cpos c, c)) $ catMaybes $ map snd parsedrow
   in (squares, creatures)
 
 parseMap :: String -> GameState
@@ -117,8 +112,8 @@ parseMap s = GameState $
     (\(sqs, crs) (y, r) ->
        let (newSquares, newCreatures) = parseRow y r
        in ( M.union sqs (M.fromList newSquares)
-          , S.union crs (S.fromList newCreatures)))
-    (M.empty, S.empty)
+          , M.union crs (M.fromList newCreatures)))
+    (M.empty, M.empty)
     (zip [0 ..] (lines s))
 
 openNeighbors :: GameState -> Pos -> [Pos]
@@ -127,7 +122,7 @@ openNeighbors (GameState (b, cs)) (Pos (x, y)) =
   in filter (\pos -> b M.! pos /= Wall && notOccupied cs pos) adjacent
 
 notOccupied :: Creatures -> Pos -> Bool
-notOccupied cs p = S.null $ S.filter (\c -> p == cpos c) cs
+notOccupied cs p = p `M.notMember` cs
 
 data PathPos = PathPos { prev    :: Maybe Pos
                        , cur     :: Pos
@@ -183,55 +178,50 @@ differentCreatureT :: Creature -> Creature -> Bool
 differentCreatureT c1 c2 = creaturet c1 /= creaturet c2
 
 round :: GameState -> GameState
-round gs@(GameState (b, cs)) = foldl' unitTurn gs (S.toAscList cs)
+round gs@(GameState (b, cs)) = foldl' unitTurn gs (M.elems cs)
 
 roundCheck :: Int -> GameState -> (Maybe (Int, Int, GameState), GameState)
 roundCheck r gs@(GameState (b, cs)) =
   foldl'
     (\(a, s@(GameState (_, css))) c ->
        ( if combatComplete s
-           then Just $ (r * (foldr (+) 0 (map chp (S.toList css))), r, s)
+           then Just $ (r * (foldr (+) 0 (map chp (M.elems css))), r, s)
            else Nothing
        , unitTurn s c))
     (Nothing, gs)
-    (S.toAscList cs)
+    (M.elems cs)
 
 unitTurn :: GameState -> Creature -> GameState
 unitTurn gs@(GameState (b, cs)) c =
-  let curCSet = S.filter (\x -> initp x == initp c) cs
-      mCurC =
-        if S.null curCSet
-          then Nothing
-          else Just $ S.elemAt 0 $ curCSet
-  in case mCurC of
-       Nothing -> gs
-       Just curC ->
-         let ts = S.filter (differentCreatureT c) cs
-             newC = unitMove curC gs ts
-             adjts = S.filter (\t -> estDist (cpos newC) (cpos t) == 1) ts
-         in if S.null adjts
-              then GameState (b, S.insert newC (S.delete curC cs))
-              else let (t:_) =
-                         sortBy
-                           (\c1 c2 ->
-                              compare (chp c1, cpos c1) (chp c2, cpos c2))
-                           (S.toList adjts)
-                       newT = newC `attack` t
-                       ncs =
-                         if chp newT <= 0
-                           then S.delete t cs
-                           else S.insert newT cs
-                   in GameState (b, S.insert newC (S.delete curC ncs))
-
+  case cs M.!? (cpos c) of
+    Nothing -> gs
+    Just curC ->
+      let ts = M.filter (differentCreatureT c) cs
+          newC = unitMove curC gs ts
+          adjts = M.filter (\t -> estDist (cpos newC) (cpos t) == 1) ts
+      in if M.null adjts
+           then GameState
+                  (b, M.insert (cpos newC) newC (M.delete (cpos curC) cs))
+           else let (t:_) =
+                      sortBy
+                        (\c1 c2 -> compare (chp c1, cpos c1) (chp c2, cpos c2))
+                        (M.elems adjts)
+                    newT = newC `attack` t
+                    ncs =
+                      if chp newT <= 0
+                        then M.delete (cpos t) cs
+                        else M.adjust (const newT) (cpos newT) cs
+                in GameState
+                     (b, M.insert (cpos newC) newC (M.delete (cpos curC) ncs))
 
 attack :: Creature -> Creature -> Creature
 attack attacker target = target {chp = (chp target) - 3}
 
-unitMove :: Creature -> GameState -> Set Creature -> Creature
+unitMove :: Creature -> GameState -> Creatures -> Creature
 unitMove c gs ts =
   if any (\t -> estDist (cpos c) (cpos t) == 1) ts
     then c -- if adjacent a target, then don't move
-    else let oInRange = concatMap (openNeighbors gs) (map cpos $ S.toList ts)
+    else let oInRange = concatMap (openNeighbors gs) (map cpos $ M.elems ts)
              paths = sort $ catMaybes $ map (shortestPath gs (cpos c)) oInRange
          in if null paths
               then c -- if no moves available, then don't move
